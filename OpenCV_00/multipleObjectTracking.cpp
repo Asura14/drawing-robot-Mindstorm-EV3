ee1683020941afbe1dc54364b6e50ec1e0da1a8b
JoãoAlmeida <alm3id4@gmail.com>
2017-04-17 16:17:14 +0100

    now tracks robot with differently colored circles on top

--- OpenCV_00/multipleObjectTracking.cpp
+++ OpenCV_00/multipleObjectTracking.cpp
@@ -15,8 +15,8 @@
 Mat cameraFeed;
 bool currentlyTrackingRobot = false;
 bool courseIsCompleted = false;
-cv::Point2f robotPosition1;
-cv::Point2f robotPosition2;
+cv::Point2f robotPosition_front;
+cv::Point2f robotPosition_rear;
 cv::Point2f robotCenter;
 cv::Point2f nextCheckpoint;
 
@@ -26,19 +26,27 @@ const float XY_DIFFERENCE_TOLERANCE = 30.0f;
 
 //initial min and max HSV filter values.
 //these will be changed using trackbars
-int H_MIN = 0;
-int H_MAX = 256;
-int S_MIN = 0;
-int S_MAX = 256;
-int V_MIN = 0;
-int V_MAX = 256;
+int H_MIN_FRONT = 0;
+int H_MAX_FRONT = 256;
+int S_MIN_FRONT = 0;
+int S_MAX_FRONT = 256;
+int V_MIN_FRONT = 0;
+int V_MAX_FRONT = 256;
+int H_MIN_REAR = 0;
+int H_MAX_REAR = 256;
+int S_MIN_REAR = 0;
+int S_MAX_REAR = 256;
+int V_MIN_REAR = 0;
+int V_MAX_REAR = 256;
 
+/*
 int H_MIN_DEFAULT = 0;
 int H_MAX_DEFAULT = 256;
 int S_MIN_DEFAULT = 49;
 int S_MAX_DEFAULT = 217;
 int V_MIN_DEFAULT = 41;
 int V_MAX_DEFAULT = 256;
+*/
 
 //default capture width and height
 //const int FRAME_WIDTH = 640;
@@ -58,9 +66,11 @@ const int MAX_OBJECT_AREA = FRAME_HEIGHT*FRAME_WIDTH/1.5;
 //names that will appear at the top of each window
 const string windowName = "Original Image";
 const string windowName1 = "HSV Image";
-const string windowName2 = "Thresholded Image";
+const string windowName2_front = "Thresholded Image FRONT";
+const string windowName2_rear = "Thresholded Image REAR";
 const string windowName3 = "After Morphological Operations";
-const string trackbarWindowName = "Trackbars";
+const string trackbarFrontWindowName = "Front Circle Tracking";
+const string trackbarRearWindowName = "Rear Circle Tracking";
 
 //The following for canny edge detection
 Mat dst, detected_edges;
@@ -70,7 +80,8 @@ int lowThreshold;
 int const max_lowThreshold = 100;
 int ratio = 3;
 int kernel_size = 3;
-const char* window_name = "Edge Map";
+const char* window_name_front = "Edge Map FRONT";
+const char* window_name_rear = "Edge Map REAR";
 
 // ---------------------------------------------- CÓDIGO BASEADO EM VCOM (INI) ----------------------------------------------
 const cv::Scalar GREEN = cv::Scalar(0, 255, 0);
@@ -148,16 +159,17 @@ string intToString(int number){
 }
 
 void createTrackbars(){
+	//  ------- Front -------
 	//create window for trackbars
-	namedWindow(trackbarWindowName,0);
+	namedWindow(trackbarFrontWindowName,0);
 	//create memory to store trackbar name on window
-	char TrackbarName[50];
-	sprintf_s( TrackbarName, "H_MIN", H_MIN);
-	sprintf_s( TrackbarName, "H_MAX", H_MAX);
-	sprintf_s( TrackbarName, "S_MIN", S_MIN);
-	sprintf_s( TrackbarName, "S_MAX", S_MAX);
-	sprintf_s( TrackbarName, "V_MIN", V_MIN);
-	sprintf_s( TrackbarName, "V_MAX", V_MAX);
+	char TrackbarName_front[50];
+	sprintf_s( TrackbarName_front, "H_MIN_FRONT", H_MIN_FRONT);
+	sprintf_s( TrackbarName_front, "H_MAX_FRONT", H_MAX_FRONT);
+	sprintf_s( TrackbarName_front, "S_MIN_FRONT", S_MIN_FRONT);
+	sprintf_s( TrackbarName_front, "S_MAX_FRONT", S_MAX_FRONT);
+	sprintf_s( TrackbarName_front, "V_MIN_FRONT", V_MIN_FRONT);
+	sprintf_s( TrackbarName_front, "V_MAX_FRONT", V_MAX_FRONT);
 	//create trackbars and insert them into window
 	//3 parameters are: the address of the variable that is changing when the trackbar is moved(eg.H_LOW),
 	//the max value the trackbar can move (eg. H_HIGH),
@@ -165,12 +177,12 @@ void createTrackbars(){
 	//                                  ---->    ---->     ---->
 
 	/**/
-	createTrackbar("H_MIN", trackbarWindowName, &H_MIN, H_MAX, on_trackbar);
-	createTrackbar("H_MAX", trackbarWindowName, &H_MAX, H_MAX, on_trackbar);
-	createTrackbar("S_MIN", trackbarWindowName, &S_MIN, S_MAX, on_trackbar);
-	createTrackbar("S_MAX", trackbarWindowName, &S_MAX, S_MAX, on_trackbar);
-	createTrackbar("V_MIN", trackbarWindowName, &V_MIN, V_MAX, on_trackbar);
-	createTrackbar("V_MAX", trackbarWindowName, &V_MAX, V_MAX, on_trackbar);
+	createTrackbar("H_MIN_FRONT", trackbarFrontWindowName, &H_MIN_FRONT, H_MAX_FRONT, on_trackbar);
+	createTrackbar("H_MAX_FRONT", trackbarFrontWindowName, &H_MAX_FRONT, H_MAX_FRONT, on_trackbar);
+	createTrackbar("S_MIN_FRONT", trackbarFrontWindowName, &S_MIN_FRONT, S_MAX_FRONT, on_trackbar);
+	createTrackbar("S_MAX_FRONT", trackbarFrontWindowName, &S_MAX_FRONT, S_MAX_FRONT, on_trackbar);
+	createTrackbar("V_MIN_FRONT", trackbarFrontWindowName, &V_MIN_FRONT, V_MAX_FRONT, on_trackbar);
+	createTrackbar("V_MAX_FRONT", trackbarFrontWindowName, &V_MAX_FRONT, V_MAX_FRONT, on_trackbar);
 	/*
 	// tentativa de atribuir valores por defeito nas trackbars:
 	createTrackbar( "H_MIN", trackbarWindowName, &H_MIN_DEFAULT, H_MAX, on_trackbar );
@@ -180,6 +192,22 @@ void createTrackbars(){
 	createTrackbar( "V_MIN", trackbarWindowName, &V_MIN_DEFAULT, V_MAX, on_trackbar );
 	createTrackbar( "V_MAX", trackbarWindowName, &V_MAX_DEFAULT, V_MAX, on_trackbar );
 	*/
+
+	//  ------- Rear -------
+	namedWindow(trackbarRearWindowName, 0);
+	char TrackbarName_rear[50];
+	sprintf_s(TrackbarName_rear, "H_MIN_REAR", H_MIN_REAR);
+	sprintf_s(TrackbarName_rear, "H_MAX_REAR", H_MAX_REAR);
+	sprintf_s(TrackbarName_rear, "S_MIN_REAR", S_MIN_REAR);
+	sprintf_s(TrackbarName_rear, "S_MAX_REAR", S_MAX_REAR);
+	sprintf_s(TrackbarName_rear, "V_MIN_REAR", V_MIN_REAR);
+	sprintf_s(TrackbarName_rear, "V_MAX_REAR", V_MAX_REAR);
+	createTrackbar("H_MIN_REAR", trackbarRearWindowName, &H_MIN_REAR, H_MAX_REAR, on_trackbar);
+	createTrackbar("H_MAX_REAR", trackbarRearWindowName, &H_MAX_REAR, H_MAX_REAR, on_trackbar);
+	createTrackbar("S_MIN_REAR", trackbarRearWindowName, &S_MIN_REAR, S_MAX_REAR, on_trackbar);
+	createTrackbar("S_MAX_REAR", trackbarRearWindowName, &S_MAX_REAR, S_MAX_REAR, on_trackbar);
+	createTrackbar("V_MIN_REAR", trackbarRearWindowName, &V_MIN_REAR, V_MAX_REAR, on_trackbar);
+	createTrackbar("V_MAX_REAR", trackbarRearWindowName, &V_MAX_REAR, V_MAX_REAR, on_trackbar);
 }
 
 void drawObject(vector<Object> theObjects,Mat &frame){
@@ -206,7 +234,7 @@ void morphOps(Mat &thresh){
 	dilate(thresh,thresh,dilateElement);
 	dilate(thresh,thresh,dilateElement);
 }
-void trackFilteredObject(Mat threshold,Mat HSV, Mat &cameraFeed)
+void trackFilteredObject(Mat threshold,Mat HSV, Mat &cameraFeed, Point2f &coords)
 {
 	vector <Object> detectedObjects;
 	Mat temp;
@@ -252,76 +280,81 @@ void trackFilteredObject(Mat threshold,Mat HSV, Mat &cameraFeed)
 				//draw object location on screen
 				drawObject(detectedObjects,cameraFeed);
 			}
-			if (detectedObjects.size() == 2)
+			if (detectedObjects.size() == 1)
 			{
-				// cout << "\n\ntwo detected objects!\n\n";
-				currentlyTrackingRobot = true;
-				robotPosition1.x = detectedObjects[0].getXPos();
-				robotPosition1.y = detectedObjects[0].getYPos();
-				robotPosition2.x = detectedObjects[1].getXPos();
-				robotPosition2.y = detectedObjects[1].getYPos();
-				robotCenter.x = (robotPosition1.x + robotPosition2.x) / 2.0f;
-				robotCenter.y = (robotPosition1.y + robotPosition2.y) / 2.0f;
-				// cout << "robot pos 1 - x = " << robotPosition1.x << ", y = " << robotPosition1.y << "\n";
-				// cout << "robot pos 2 - x = " << robotPosition2.x << ", y = " << robotPosition2.y << "\n";
-				cout << "\n\nrobot center - x = " << robotPosition2.x << ", y = " << robotPosition2.y << "\n";
-
-
-				float angle1 = atan((robotPosition2.y - robotPosition1.y) / (robotPosition2.x - robotPosition1.y)) * 180/PI;
-				float angle2 = atan((robotPosition1.y - robotPosition1.y) / (robotPosition2.x - robotPosition2.y)) * 180 / PI;
-				float nextAngle = -1;
-
-				// convert to angle with horizontal
-				// angle1 -= 90;
-				// angle2 -= 90;
-
-				cout << "angle 1 = " << angle1 << endl;
-				// cout << "angle 2 = " << angle1 << endl;
-
-				if (!courseIsCompleted) {
-					nextCheckpoint.x = course[nextCheckpointIndex].x;
-					nextCheckpoint.y = course[nextCheckpointIndex].y;
-					cout << "nextCheckpoint x = " << nextCheckpoint.x << ", y = " << nextCheckpoint.y << "\n";
-					nextAngle = atan((nextCheckpoint.y - robotCenter.y) / (nextCheckpoint.x - robotCenter.y)) * 180 / PI;
-					// convert to angle with horizontal
-					// nextAngle -= 90;
-
-					cout << "nextAngle = " << nextAngle << endl;
-					float distanceToCheckpoint = distance(robotCenter, nextCheckpoint);
-					float angleDifference = nextAngle - angle1;
-					cout << "angle difference = " << angleDifference << endl;
-
-					if (distanceToCheckpoint < XY_DIFFERENCE_TOLERANCE)
-						cout << "REACHED_CHECKPOINT" << endl << "STOP" << endl;
-					else
-						if (abs(angleDifference) < ANGLE_DIFFERENCE_TOLERANCE)
-							cout << "GO_FORWARD";
-						else if (angleDifference > 0)
-							cout << "ROTATE_RIGHT";
-						else
-							cout << "ROTATE_LEFT";
-						
-					
-					// check to see if robot within tolerance range for next spot
-					if ((abs(robotCenter.x - nextCheckpoint.x) < XY_DIFFERENCE_TOLERANCE)
-						&& (abs(robotCenter.y - nextCheckpoint.y) < XY_DIFFERENCE_TOLERANCE)) {
-						
-						courseCheckpoints[nextCheckpointIndex] = true; // validates reached checkpoint
-						cout << " --- Hit checkpoint i = " << nextCheckpointIndex << endl;
-						if (nextCheckpointIndex == course.size() - 1)
-							courseIsCompleted = true; // course is complete once all the course's checkpoints are ran
-						else
-						{
-							nextCheckpointIndex++;	
-							nextCheckpoint = course[nextCheckpointIndex];
-						}
-					}
-				}
+				coords.x = detectedObjects[0].getXPos();
+				coords.y = detectedObjects[0].getYPos();
+				//cout << "robot pos 1 - x = " << robotPosition1.x << ", y = " << robotPosition1.y << "\n";
 			}
 			else
 				currentlyTrackingRobot = false;
 		}
 		else putText(cameraFeed,"TOO MUCH NOISE! ADJUST FILTER",Point(0,50),1,2,Scalar(0,0,255),2);
+	}
+}
+
+void robot()
+{
+	// cout << "\n\ntwo detected objects!\n\n";
+	//currentlyTrackingRobot = true;
+	//robotPosition1.x = detectedObjects[0].getXPos();
+	//robotPosition1.y = detectedObjects[0].getYPos();
+	//robotPosition2.x = detectedObjects[1].getXPos();
+	//robotPosition2.y = detectedObjects[1].getYPos();
+	robotCenter.x = (robotPosition_front.x + robotPosition_rear.x) / 2.0f;
+	robotCenter.y = (robotPosition_front.y + robotPosition_rear.y) / 2.0f;
+
+	// cout << "robot pos 2 - x = " << robotPosition2.x << ", y = " << robotPosition2.y << "\n";
+	cout << "\n\nrobot center - x = " << robotCenter.x << ", y = " << robotCenter.y << "\n";
+
+
+	float angle1 = atan((robotPosition_front.y - robotPosition_rear.y) / (robotPosition_front.x - robotPosition_rear.x)) * 180/PI;
+	cout << "\nAngle 1 = " << angle1 << endl;
+
+	float nextAngle = -1;
+
+	// convert to angle with horizontal
+	// angle1 -= 90;
+	// angle2 -= 90;	
+
+	if (!courseIsCompleted) {
+		nextCheckpoint.x = course[nextCheckpointIndex].x;
+		nextCheckpoint.y = course[nextCheckpointIndex].y;
+		cout << "nextCheckpoint x = " << nextCheckpoint.x << ", y = " << nextCheckpoint.y << "\n";
+		nextAngle = atan((nextCheckpoint.y - robotCenter.y) / (nextCheckpoint.x - robotCenter.x)) * 180 / PI;
+
+		cout << "nextAngle = " << nextAngle << endl;
+		float distanceToCheckpoint = distance(robotCenter, nextCheckpoint);
+		float angleDifference = nextAngle - angle1;
+		cout << "angle difference = " << angleDifference << endl;
+
+		
+		// Print instructions for robot
+		if (distanceToCheckpoint < XY_DIFFERENCE_TOLERANCE)
+		cout << "REACHED_CHECKPOINT" << endl << "STOP" << endl;
+		else
+		if (abs(angleDifference) < ANGLE_DIFFERENCE_TOLERANCE)
+		cout << "GO_FORWARD";
+		else if (angleDifference > 0)
+		cout << "ROTATE_RIGHT";
+		else
+		cout << "ROTATE_LEFT";
+		
+
+		// check to see if robot within tolerance range for next spot
+		if ((abs(robotCenter.x - nextCheckpoint.x) < XY_DIFFERENCE_TOLERANCE)
+			&& (abs(robotCenter.y - nextCheckpoint.y) < XY_DIFFERENCE_TOLERANCE)) {
+
+			courseCheckpoints[nextCheckpointIndex] = true; // validates reached checkpoint
+			cout << " --- Hit checkpoint i = " << nextCheckpointIndex << endl;
+			if (nextCheckpointIndex == course.size() - 1)
+				courseIsCompleted = true; // course is complete once all the course's checkpoints are ran
+			else
+			{
+				nextCheckpointIndex++;
+				nextCheckpoint = course[nextCheckpointIndex];
+			}
+		}
 	}
 }
 
@@ -337,8 +370,10 @@ int main(int argc, char* argv[])
 
 	//Matrix to store each frame of the webcam feed
 
-	Mat threshold;
-	Mat HSV;
+	Mat threshold_front;
+	Mat threshold_rear;
+	Mat HSV_front;
+	Mat HSV_rear;
 
 	if(calibrationMode){
 		//create slider bars for HSV filtering
@@ -370,7 +405,8 @@ int main(int argc, char* argv[])
   		{ return -1; }
 
 		//convert frame from BGR to HSV colorspace
-		cvtColor(cameraFeed,HSV,COLOR_BGR2HSV);
+		cvtColor(cameraFeed,HSV_front,COLOR_BGR2HSV);
+		cvtColor(cameraFeed, HSV_rear, COLOR_BGR2HSV);
 
 		// ------------- MODO DE HOMOGRAFIA -------------
 		if (homographyMode == true)
@@ -432,11 +468,13 @@ int main(int argc, char* argv[])
 				warpedImage.copyTo(roi);
 
 				// -------------- detecção de objetos (INI) --------------
+
+				// ------- FRONT -------
 				//if in calibration mode, we track objects based on the HSV slider values.
-				cvtColor(roi, HSV, COLOR_BGR2HSV);
-				inRange(HSV, Scalar(H_MIN, S_MIN, V_MIN), Scalar(H_MAX, S_MAX, V_MAX), threshold);
-				morphOps(threshold);
-				imshow(windowName2, threshold); // thresholded image
+				cvtColor(roi, HSV_front, COLOR_BGR2HSV);
+				inRange(HSV_front, Scalar(H_MIN_FRONT, S_MIN_FRONT, V_MIN_FRONT), Scalar(H_MAX_FRONT, S_MAX_FRONT, V_MAX_FRONT), threshold_front);
+				morphOps(threshold_front);
+				imshow(windowName2_front, threshold_front); // thresholded image
 
 				//the folowing for canny edge detec
 				/// Create a matrix of the same type and size as src (for dst)
@@ -444,11 +482,32 @@ int main(int argc, char* argv[])
 				/// Convert the image to grayscale
 				cvtColor(src, src_gray, CV_BGR2GRAY);
 				/// Create a window
-				namedWindow(window_name, CV_WINDOW_AUTOSIZE);
+				namedWindow(window_name_front, CV_WINDOW_AUTOSIZE);
 				/// Create a Trackbar for user to enter threshold
-				createTrackbar("Min Threshold:", window_name, &lowThreshold, max_lowThreshold);
+				createTrackbar("Min Threshold:", window_name_front, &lowThreshold, max_lowThreshold);
 				/// Show the image
-				trackFilteredObject(threshold, HSV, roi);
+				trackFilteredObject(threshold_front, HSV_front, roi, robotPosition_front);
+
+				// ------- REAR -------
+				//if in calibration mode, we track objects based on the HSV slider values.
+				cvtColor(roi, HSV_rear, COLOR_BGR2HSV);
+				inRange(HSV_rear, Scalar(H_MIN_REAR, S_MIN_REAR, V_MIN_REAR), Scalar(H_MAX_REAR, S_MAX_REAR, V_MAX_REAR), threshold_rear);
+				morphOps(threshold_rear);
+				imshow(windowName2_rear, threshold_rear); // thresholded image
+
+												//the folowing for canny edge detec
+												/// Create a matrix of the same type and size as src (for dst)
+				dst.create(src.size(), src.type());
+				/// Convert the image to grayscale
+				cvtColor(src, src_gray, CV_BGR2GRAY);
+				/// Create a window
+				namedWindow(window_name_rear, CV_WINDOW_AUTOSIZE);
+				/// Create a Trackbar for user to enter threshold
+				createTrackbar("Min Threshold:", window_name_rear, &lowThreshold, max_lowThreshold);
+				/// Show the image
+				trackFilteredObject(threshold_rear, HSV_rear, roi, robotPosition_rear);
+
+				robot();
 
 				// -------------- detecção de objetos (FIM) --------------
 
